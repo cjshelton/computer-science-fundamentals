c657957e5dda495ace4df9dbd28504c3f2d1015f
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2020-05-25 19:19:26 +0100

    Added delete item function to the Singly Linked List.

--- src/node/src/linked-lists/SinglyLinkedList.ts
+++ src/node/src/linked-lists/SinglyLinkedList.ts
@@ -27,7 +27,7 @@ class SinglyLinkedList<T> {
 
     pop(): T {
         if (this.isEmpty()) {
-            return null;
+            throw new Error('Cannot pop from the linked list. It is empty.');
         } else if (this.length === 1) {
             const item = this.tail.value;
             this.head = null;
@@ -81,6 +81,76 @@ class SinglyLinkedList<T> {
         }
 
         return currentNode.value;
+    }
+
+    delete(index: number): void {
+        if (this.isEmpty() || index < 0 || index >= this.length) {
+            throw new Error(
+                `Cannot delete item at index ${index}. It is out of bounds.`
+            );
+        }
+
+        // If the first item is requested, we can skip traversing through the
+        // list and just update the head.
+        if (index === 0) {
+            if (this.length === 1) {
+                this.head = null;
+                this.tail = null;
+            } else {
+                this.head = this.head.next;
+                this.length--;
+            }
+
+            return;
+        }
+
+        // The node before the one to delete is needed to the next pointer can be updated.
+        const nodeBeforeIndex = this.getNodeBeforeIndex(index);
+        const nodeToRemove = nodeBeforeIndex.next;
+
+        // Check if the index is for the last node.
+        if (index === this.length - 1) {
+            // Set the second to last node as the new tail.
+            nodeBeforeIndex.next = null;
+            this.tail = nodeBeforeIndex;
+            this.length--;
+
+            return;
+        }
+
+        // Otherwise, set the next pointer to point to the node after the one to be deleted.
+        nodeBeforeIndex.next = nodeToRemove.next;
+        this.length--;
+    }
+
+    private getNodeBeforeIndex(index: number): SinglyLinkedListNode<T> {
+        if (this.length <= 1) {
+            throw new Error(
+                `Cannot get node before index ${index}. List must have at least 2 items.`
+            );
+        }
+
+        if (index >= this.length) {
+            throw new Error(
+                `Cannot get node before index ${index}. Index is out of bounds.`
+            );
+        }
+
+        // The node before the first index is just the head.
+        if (index === 1) {
+            return this.head;
+        }
+
+        let currentNode = this.head;
+        let currentIndex = 0;
+
+        while (currentIndex < index) {
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+
+        // Updated the penultimate to be the new tail node.
+        return currentNode;
     }
 
     isEmpty(): boolean {
--- src/node/tests/data-structures/linked-lists/singlyLinkedList.test.ts
+++ src/node/tests/data-structures/linked-lists/singlyLinkedList.test.ts
@@ -35,12 +35,10 @@ describe('SinglyLinkedList', () => {
     });
 
     describe('pop', () => {
-        test('it returns null when no items are in the list', () => {
+        test('it fails when the list is empty', () => {
             const linkedList = new SinglyLinkedList<number>();
 
-            const poppedItem: number = linkedList.pop();
-
-            expect(poppedItem).toBeNull();
+            expect(() => linkedList.pop()).toThrow();
         });
 
         test('it returns the item when only one item exists in the list', () => {
@@ -89,20 +87,6 @@ describe('SinglyLinkedList', () => {
     });
 
     describe('get', () => {
-        test('it fails when the list is empty', () => {
-            const linkedList = new SinglyLinkedList<number>();
-
-            expect(() => linkedList.get(0)).toThrow();
-        });
-
-        test('it fails when the index is out of bounds', () => {
-            const linkedList = new SinglyLinkedList<number>();
-            linkedList.push(1);
-
-            expect(() => linkedList.get(-1)).toThrow();
-            expect(() => linkedList.get(1)).toThrow();
-        });
-
         test('it gets the item at the index', () => {
             const linkedList = new SinglyLinkedList<number>();
             linkedList.push(1);
@@ -116,6 +100,89 @@ describe('SinglyLinkedList', () => {
             expect(firstItem).toBe(1);
             expect(secondItem).toBe(2);
             expect(thirdItem).toBe(3);
+        });
+
+        test('it fails when the list is empty', () => {
+            const linkedList = new SinglyLinkedList<number>();
+
+            expect(() => linkedList.get(0)).toThrow();
+        });
+
+        test('it fails when the index is out of bounds', () => {
+            const linkedList = new SinglyLinkedList<number>();
+            linkedList.push(1);
+
+            expect(() => linkedList.get(-1)).toThrow();
+            expect(() => linkedList.get(1)).toThrow();
+        });
+    });
+
+    describe('delete', () => {
+        test('it deletes the item when there is only one item', () => {
+            const linkedList = new SinglyLinkedList<object>();
+            linkedList.push({
+                foo: 'bar',
+            });
+
+            linkedList.delete(0);
+
+            expect(linkedList.isEmpty).toBeTruthy();
+            expect(linkedList.head).toBeNull();
+            expect(linkedList.tail).toBeNull();
+        });
+
+        test('it fails when the list is empty', () => {
+            const linkedList = new SinglyLinkedList<number>();
+
+            expect(() => linkedList.delete(0)).toThrow();
+        });
+
+        test('it fails when the index is out of bounds', () => {
+            const linkedList = new SinglyLinkedList<number>();
+            linkedList.push(1);
+
+            expect(() => linkedList.delete(-1)).toThrow();
+            expect(() => linkedList.delete(1)).toThrow();
+        });
+
+        test('it updates the head when the first item is deleted', () => {
+            const linkedList = new SinglyLinkedList<object>();
+
+            const itemOne = { foo: 'bar' };
+            const itemTwo = { bar: 'foo' };
+            linkedList.push(itemOne);
+            linkedList.push(itemTwo);
+
+            linkedList.delete(0);
+
+            expect(linkedList.length).toBe(1);
+            expect(Object.is(linkedList.head.value, itemTwo)).toBe(true);
+        });
+
+        test('it updates the tail when the last item is deleted', () => {
+            const linkedList = new SinglyLinkedList<object>();
+
+            const itemOne = { foo: 'bar' };
+            const itemTwo = { bar: 'foo' };
+            linkedList.push(itemOne);
+            linkedList.push(itemTwo);
+
+            linkedList.delete(1);
+
+            expect(linkedList.length).toBe(1);
+            expect(Object.is(linkedList.tail.value, itemOne)).toBe(true);
+        });
+
+        test('it deletes the item at the index from the middle of the list', () => {
+            const linkedList = new SinglyLinkedList<number>();
+            linkedList.push(1);
+            linkedList.push(2);
+            linkedList.push(3);
+
+            linkedList.delete(1);
+
+            expect(linkedList.length).toBe(2);
+            expect(linkedList.get(1)).toBe(3);
         });
     });
 });
